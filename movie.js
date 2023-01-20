241ed84ae0405aa8b9fca7ebbcd3622bac810216
An10113 <antranthanh1011@gmail.com>
2023-01-20 23:55:23 +0700

    6

--- movie.js
+++ movie.js
@@ -3,7 +3,20 @@
 async function main (){
     let movie = await fetch("https://www.omdbapi.com/?apikey=a020a8a2&s=rise")
     let moviedata = await movie.json()
-    console.log(moviedata)
+    
+}
+
+function movieHTML(data){
+    return  `<div class="movie">
+              <div class="movie__img--wrapper">
+                <img src="${data.Poster}" alt="" />
+              </div>
+              <div class="movie__description">
+                <div class="movie__title">${data.Title}</div>
+                <div class="movie__year">${data.Year}</div>
+                <div class="movie__ID">${data.imdbID}</div>
+              </div>
+            </div>`
 }
 
 main()
